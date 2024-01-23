114db9f7be9f4757df8337d9a2b22d390439b069
ihebmechi <ihebmechi4@gmail.com>
2024-01-23 14:18:03 +0100

    second commit

---
+++ Programming Basics/feature2.js
@@ -0,0 +1,7 @@
+if(Today=" Time 3Pm=>6Pm "){
+    (Eat_Chocolate,Ice_Cream)
+if(TheTime=" 3Pm or 5Pm ")
+    (Eat_Chocolate)
+if(TheTime=" 4Pm or 6Pm ")
+    (Eat_IceCream)
+}
---
+++ Programming Basics/future3.js
@@ -0,0 +1,5 @@
+if(Today=" The Time 3Pm=>6Pm "){
+    (Eat_Ice_Cream,Cookies_Or_Candy)
+if(Today=" The Time Not On 3Pm=>6Pm ")
+    (Eat_Hot_Chocolate,Tea_Or_Cake)
+}
