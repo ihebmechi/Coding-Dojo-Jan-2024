45ffe4360cc328c5c744d6c3bd841bb6e3f55fea
ihebmechi <157384720+ihebmechi@users.noreply.github.com>
2024-02-07 13:52:27 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-console.log("page loaded...");
-function play(){
-    var x=document.getElementById("vd");
-    x.play();
-}function pause(){
-    var x=document.getElementById("vd");
-    x.pause();
+function pizzaOven(crustType, sausType, cheeses, toppings) {
+    var pizza ={};
+    pizza.crustType = crustType;
+    pizza.sausType = sausType;
+    pizza.cheeses = cheeses;
+    pizza.toppings = toppings;
+    return pizza;
 }
+pizza1={
+    crustType : "deep dish",
+    sausType : "traditionnel",
+    cheeses : ["mozzarella"],
+    toppings : ["pepperoni","sausage"],
+}
+console.log (pizza1);
+pizza2={
+    crustType : "hand tossed",
+    sausType : "marinara",
+    cheeses :["mozzarella","feta"],
+    toppings : ["mushrooms","olives","onions"],
+}    
+console.log (pizza2);
+pizza3={
+    crustType : "hand tossed",
+    sausType : "marinara",
+    cheeses : ["mozzarella"],
+    toppings : ["mushrooms","pepperoni","onions","sausage"],
+}    
+console.log (pizza3);
+
+
